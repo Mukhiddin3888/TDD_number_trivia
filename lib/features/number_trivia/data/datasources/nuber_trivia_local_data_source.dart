dccc80861c4c23630c80599fc14a56e8fb51b546
Mukhiddin3888 <sirojiddinovm720@mail.ru>
2021-11-04 21:03:41 +0500

    feat: add data process

---
+++ lib/features/number_trivia/data/datasources/nuber_trivia_local_data_source.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:tdd_test/features/number_trivia/data/models/number_trivia_model.dart';
+
+abstract class NumberTriviaLocalDataSource{
+
+
+Future<NumberTriviaModel> getLastNumberTrivia();
+
+Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+
+}
